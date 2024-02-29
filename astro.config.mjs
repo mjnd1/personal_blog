24cff89c39ff3b3f0ca112cb2afaa0e58e340558
chenzhiwei <577514793@qq,com>
2024-02-29 15:02:13 +0800

    Astro岛屿：引入使用Preact框架，使用client:指令

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // 在 Astro 的配置文件中设置 site 属性为你的网站自己的唯一 Netlify URL
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-})
+  integrations: [preact()]
+});
